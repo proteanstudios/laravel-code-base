7bddb0be1d4855aed81eb9f2b3e36321afac805c
Leo <vuongvankhanh89@gmail.com>
2021-12-03 14:48:46 +0700

    allow ApiLogicException throw customized status code

--- src/Exceptions/ApiLogicException.php
+++ src/Exceptions/ApiLogicException.php
@@ -6,5 +6,16 @@ use Exception;
 
 class ApiLogicException extends Exception
 {
-    //
+    protected $status = 'ERR';
+
+    public function __construct($message, $status = 'ERR', $code = 401)
+    {
+        $this->status = $status;
+        parent::__construct(message: $message, code: $code);
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
 }
